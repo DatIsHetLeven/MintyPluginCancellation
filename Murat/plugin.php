2fa076acae2e4c76070e5ec84a8a604574d31ce7
DatIsHetLeven <muratcelem187@gmail.com>
2022-12-05 15:16:20 +0100

    Update
    Als item op geannuleerd staat terwijl maar 1 item gean is -> status updte deels geann

--- Murat/plugin.php
+++ Murat/plugin.php
@@ -25,32 +25,110 @@ Domain Path:  /languages
 // TODO: Aangeven in de achterkant welke producten geanuleerd zijn.
 
 //TODO ; een nieuwe order status aanmaken -> naam : Gedeeltelijk geretourneerd'
-// Register New Order Statuses
-function wpex_wc_register_post_statuses() {
-	register_post_status( 'wc-custom-order-status', array(
-		'label'						=> _x( 'Custom Order Status Name', 'WooCommerce Order status', 'text_domain' ),
-		'public'					=> true,
-		'exclude_from_search'		=> false,
-		'show_in_admin_all_list'	=> true,
-		'show_in_admin_status_list'	=> true,
-		'label_count'				=> _n_noop( 'Approved (%s)', 'Approved (%s)', 'text_domain' )
+
+
+// Register new status
+function register_wait_call_order_status() {
+	register_post_status( 'wc-waiting-call', array(
+		'label'                     => 'Waiting call',
+		'public'                    => true,
+		'show_in_admin_status_list' => true,
+		'show_in_admin_all_list'    => true,
+		'exclude_from_search'       => false,
+		'label_count'               => _n_noop( 'Waiting call (%s)', 'Waiting call (%s)' )
 	) );
 }
-add_filter( 'init', 'wpex_wc_register_post_statuses' );
+// Add custom status to order status list
+function add_wait_call_to_order_statuses( $order_statuses ) {
+	$new_order_statuses = array();
+	foreach ( $order_statuses as $key => $status ) {
+		$new_order_statuses[ $key ] = $status;
+		if ( 'wc-on-hold' === $key ) {
+			$new_order_statuses['wc-waiting-call'] = 'Deels geannuleerd';
+		}
+	}
+	return $new_order_statuses;
+}
+add_action( 'init', 'register_wait_call_order_status' );
+add_filter( 'wc_order_statuses', 'add_wait_call_to_order_statuses' );
 
-// Add New Order Statuses to WooCommerce
-function wpex_wc_add_order_statuses( $order_statuses ) {
-	$order_statuses['wc-custom-order-status'] = _x( 'Custom Order Status Name', 'WooCommerce Order status', 'text_domain' );
+function QuadLayers_rename_status( $order_statuses ) {
+	foreach ( $order_statuses as $key => $status ) {
+		if ( 'wc-processing' === $key ) {
+			$order_statuses['wc-processing'] = _x( 'In progress', 'Order status', 'woocommerce' );
+		}
+		if ( 'wc-completed' === $key ) {
+			$order_statuses['wc-completed'] = _x( 'Delivered', 'Order status', 'woocommerce' );
+		}
+	}
 	return $order_statuses;
 }
-add_filter( 'wc_order_statuses', 'wpex_wc_add_order_statuses' );
+add_filter( 'wc_order_statuses', 'QuadLayers_rename_status' );
 
-//TODO: de status moet alleen gelden voor half geanullerde bestellingen
-add_action('woocommerce_order_status_changed', 'checkOrderStatus');
 
-function checkOrderStatus(){
+
+
+
+
+
+
+
+add_action( 'wc-waiting-call', 'wpdesk_set_completed_for_paid_orders' );
+
+function wpdesk_set_completed_for_paid_orders( $order_id ) {
+dd("testetstest");
+	$order = wc_get_order( $order_id );
+	echo "<h1> testttddddddddddddt</h1>";
+	dump($order);
 
 }
 
+add_action('init',function(){
+	$order = new WC_Order(23);
 
+//Als de order op geannuleerd staat
+	if ($order->get_status() == 'cancelled'){
+		$order = wc_get_order( 23 );
+		$order_refunds = $order->get_refunds();
+		// Loop through the order refunds array
+		foreach( $order_refunds as $refund ){
+			// Loop through the order refund line items
+			foreach( $refund->get_items() as $item_id => $item ){
+
+				## --- Using WC_Order_Item_Product methods --- ##
+
+				$refunded_quantity      = $item->get_quantity(); // Quantity: zero or negative integer
+				$refunded_line_subtotal = $item->get_subtotal(); // line subtotal: zero or negative number
+				$ordernaam = $item->get_name();
+				dump($ordernaam);
+				dump($item);
+				dump($refunded_quantity);
+				dump($refunded_line_subtotal);
+				// ... And so on ...
+
+				// Get the original refunded item ID
+				$refunded_item_id       = $item->get_meta('_refunded_item_id'); // line subtotal: zero or negative number
+
+				$order->update_status('wc-waiting-call');
+			}
+		}
+
+
+
+		//Check of alle items gean. zijn
+		//pas staus aan naar 'deels-gan. if yes'
+	}
+
+	if ($order->get_status() == 'waiting-call'){
+		echo "<h1>dddhdhjgdjhgdhkdgdhgh</h1>";
+	}
+	else{
+		echo "<h1>nenenenenenenenenenenenenenenne</h1>";
+	}
+
+
+
+
+//	$order->update_status('wc-waiting-call');
+});
 
